05e41bc228c83f834f0d470400db97f9a9ff7df8
kolayma <kolayma@gmail.com>
2023-10-03 23:04:01 +1100

    added function map and deleted <li>

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ function App() {
     let task1: Array<TaskType> = [
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "React", isDone: false}
+        {id: 3, title: "React", isDone: false},
+        {id: 4, title: "Redux", isDone: false}
 
     ]
 
     let task2: Array<TaskType> = [
-        {id: 1, title: "Terminator", isDone: true},
-        {id: 2, title: "XXX", isDone: false},
-        {id: 3, title: "Jentlments of fortune", isDone: true}
+        {id: 1, title: "Terminator", isDone: false},
+        {id: 2, title: "XXX", isDone: false}
 
     ]
 
